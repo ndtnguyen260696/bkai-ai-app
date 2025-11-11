88bb0e93760879a7171a73f48ea97f42413f7898
ndtnguyen260696 <nguyendatthanh26061996@gmail.com>
2025-11-11 12:35:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -260,20 +260,20 @@ def export_pdf(original_img, analyzed_img, metrics_df, filename="bkai_report.pdf
 
         # Các dòng dữ liệu: dùng Paragraph để tự wrap + RÚT GỌN mô tả
         for _, row in metrics_df.iterrows():
-           vi_txt = Paragraph(str(row["vi"]), normal)
-           en_txt = Paragraph(str(row["en"]), normal)
-           val_txt = Paragraph(str(row["value"]), normal)
+    vi_txt = Paragraph(str(row["vi"]), normal)
+    en_txt = Paragraph(str(row["en"]), normal)
+    val_txt = Paragraph(str(row["value"]), normal)
 
-        # RÚT GỌN MÔ TẢ CHO PDF để mỗi ô không quá cao
-          full_desc = str(row["desc"])
-       if len(full_desc) > 180:      # muốn ngắn hơn nữa thì giảm 180
+    # RÚT GỌN MÔ TẢ CHO PDF để mỗi ô không quá cao
+    full_desc = str(row["desc"])
+    if len(full_desc) > 180:      # muốn ngắn hơn nữa thì giảm 180
         short_desc = full_desc[:180] + "..."
-       else:
-            short_desc = full_desc
+    else:
+        short_desc = full_desc
 
-            desc_txt = Paragraph(short_desc, normal)
+    desc_txt = Paragraph(short_desc, normal)
 
-            data.append([vi_txt, en_txt, val_txt, desc_txt])
+    data.append([vi_txt, en_txt, val_txt, desc_txt])
     
 
         col_widths = [
@@ -737,6 +737,7 @@ if st.session_state.authenticated:
     run_main_app()
 else:
     show_auth_page()
+
 
 
 
