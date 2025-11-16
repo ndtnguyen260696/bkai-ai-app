cb894649db747061402f8a855a6808ac48a0c2c9
ndtnguyen260696 <nguyendatthanh26061996@gmail.com>
2025-11-17 05:58:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1617,13 +1617,18 @@ def show_auth_page():
             st.markdown("### BKAI")
 
     with col_header:
-        st.title("BKAI - MÔ HÌNH CNN PHÁT HIỆN VÀ PHÂN LOẠI VẾT NỨT BÊ TÔNG")
-        st.markdown(
-            "<p style='font-size:16px;color:#555;'>"
-            "Vui lòng đăng nhập hoặc đăng ký để sử dụng hệ thống phân tích vết nứt bê tông."
-            "</p>",
-            unsafe_allow_html=True,
-        )
+    st.markdown(
+        "<h2 style='margin:5px 0 5px 0; color:#333;'>"
+        "BKAI - MÔ HÌNH CNN PHÁT HIỆN VÀ PHÂN LOẠI VẾT NỨT BÊ TÔNG"
+        "</h2>",
+        unsafe_allow_html=True,
+    )
+    st.markdown(
+        "<p style='font-size:15px; color:#555;'>"
+        "Vui lòng đăng nhập hoặc đăng ký để sử dụng hệ thống."
+        "</p>",
+        unsafe_allow_html=True,
+    )
 
     st.write("---")
 
@@ -1678,5 +1683,6 @@ if st.session_state.authenticated:
     run_main_app()
 else:
     show_auth_page()
+
 
 
